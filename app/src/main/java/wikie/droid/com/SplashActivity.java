264111a3fd864c08286ff97647cf0340fadd7161
cngondo <devngondo@gmail.com>
2016-08-04 22:25:50 +0300

    splash screen

--- app/src/main/java/wikie/droid/com/SplashActivity.java
+++ app/src/main/java/wikie/droid/com/SplashActivity.java
@@ -2,12 +2,21 @@ package wikie.droid.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
+    private static  int INTERVAL = 2500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //remove titlebar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+        //hide the action bar
+        getSupportActionBar().hide();
     }
 }
